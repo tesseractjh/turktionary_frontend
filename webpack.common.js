dca83b97a4d73eaa7fe5fc37fc54ab03d0d8d89a
Tesseract <tensai3355@gmail.com>
2022-08-31 14:05:58 +0900

    :wrench: api 절대경로 추가

--- webpack.common.js
+++ webpack.common.js
@@ -9,6 +9,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
     alias: {
+      '@api': path.resolve(__dirname, 'src/api'),
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@contexts': path.resolve(__dirname, 'src/contexts'),
