132b47cd76c0d82f596c6a18393947e930b4b14b
Tesseract <tensai3355@gmail.com>
2022-09-04 03:06:45 +0900

    :adhesive_bandage: 로그인 여부에 따라 메뉴가 변하도록 수정

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react';
 import styled from '@emotion/styled';
 import { flex } from '@styles/minxin';
 import pxToRem from '@utils/pxToRem';
 import UserIcon from '@assets/images/user-solid.svg';
+import LoginIcon from '@assets/images/arrow-right-to-bracket-solid.svg';
 import InnerContainer from '@components/common/InnerContainer';
 import Logo from '@components/common/Logo';
 import MenuButton from './MenuButton';
+import { useEffect, useState } from 'react';
+import useAPI from '@hooks/useAPI';
+import userAPI from '@api/user';
+import { useRecoilValue } from 'recoil';
+import { accessTokenState } from '@recoil/user';
 
 const Container = styled.nav`
   height: ${pxToRem(60)};
@@ -19,19 +24,42 @@ const Content = styled.div`
 
 const HeaderMenu = styled.ul`
   ${flex('flex-end')}
+  gap: ${pxToRem(20)};
 `;
 
 function Header() {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const accessToken = useRecoilValue(accessTokenState);
+  const [userData, setUserData] = useState<Pick<
+    Model.User,
+    'user_name' | 'user_exp'
+  > | null>(null);
+  const api = useAPI();
+
+  useEffect(() => {
+    if (accessToken) {
+      (async () => {
+        await api(userAPI.getHeaderUserInfo(), (userData) => {
+          setUserData(userData);
+        })();
+      })();
+    }
+  }, [accessToken]);
+
   return (
     <Container>
       <InnerContainer>
         <Content>
           <Logo />
           <HeaderMenu role="menu">
-            <MenuButton text={isLoggedIn ? '로그아웃' : '로그인'}>
-              <UserIcon />
-            </MenuButton>
+            {userData ? (
+              <MenuButton text="내 정보">
+                <UserIcon />
+              </MenuButton>
+            ) : (
+              <MenuButton text="로그인">
+                <LoginIcon />
+              </MenuButton>
+            )}
           </HeaderMenu>
         </Content>
       </InnerContainer>
