9814753762728cbb96f48b7f7a1c4e522260a705
Tesseract <tensai3355@gmail.com>
2022-09-04 02:58:20 +0900

    :adhesive_bandage: Link 대신 a태그를 사용할 수 있도록 수정

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -3,6 +3,11 @@ import styled from '@emotion/styled';
 import pxToRem from '@utils/pxToRem';
 import LogoIcon from '@assets/images/star-and-crescent-solid.svg';
 
+interface LogoProps {
+  useHref?: boolean;
+  className?: string;
+}
+
 const LogoLink = styled(Link)`
   font-weight: 600;
   font-size: ${pxToRem(28)};
@@ -14,13 +19,35 @@ const LogoLink = styled(Link)`
   }
 `;
 
-function Logo() {
+const LogoAnchor = styled.a`
+  font-weight: 600;
+  font-size: ${pxToRem(28)};
+
+  & svg {
+    width: ${pxToRem(20)};
+    height: ${pxToRem(20)};
+    margin-right: ${pxToRem(10)};
+  }
+`;
+
+function Logo({ useHref = false, className }: LogoProps) {
+  if (useHref) {
+    return (
+      <h1 className={className}>
+        <LogoLink to="/">
+          <LogoIcon />
+          Turktionary
+        </LogoLink>
+      </h1>
+    );
+  }
+
   return (
-    <h1>
-      <LogoLink to="/">
+    <h1 className={className}>
+      <LogoAnchor href="/">
         <LogoIcon />
         Turktionary
-      </LogoLink>
+      </LogoAnchor>
     </h1>
   );
 }
