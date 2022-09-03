2ad426c3751b788d8967684cc850ae4a923cfdfa
Tesseract <tensai3355@gmail.com>
2022-09-04 03:02:18 +0900

    :label: DB의 User 테이블 타입 추가

---
+++ @types/model.d.ts
@@ -0,0 +1,12 @@
+declare namespace Model {
+  interface User {
+    user_id: number;
+    auth_id: string;
+    auth_provider: string;
+    user_name: string;
+    user_exp: number;
+    introduction: string | null;
+    email: string | null;
+    refresh_token: string | null;
+  }
+}
