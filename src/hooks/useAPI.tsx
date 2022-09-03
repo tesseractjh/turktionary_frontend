a36cd3a62876be9a02769c75c77a44eada44c58f
Tesseract <tensai3355@gmail.com>
2022-09-04 03:04:00 +0900

    :sparkles: API 에러 처리를 위한 훅 추가

---
+++ src/hooks/useAPI.tsx
@@ -0,0 +1,54 @@
+import { AxiosError } from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
+import { accessTokenState } from '@recoil/user';
+
+const errorMap: Partial<Record<ErrorCode, string>> = {
+  '000': '알 수 없는 오류가 발생하였습니다!',
+  '001': '로그인이 필요합니다!',
+  '002': '자동 로그인 기간이 만료되어 로그아웃되었습니다!',
+  '003': '회원가입 도중 오류가 발생하였습니다! 다시 시도해주세요.',
+  '004': '회원가입 도중 오류가 발생하였습니다! 다시 시도해주세요.',
+  '005': '유효하지 않은 값이 입력되었습니다! 다시 시도해주세요.',
+  '006': '회원가입 도중 오류가 발생하였습니다! 다시 시도해주세요.',
+  '999': '서버와의 연결이 끊어졌습니다!'
+};
+
+function useAPI() {
+  const setAccessToken = useSetRecoilState(accessTokenState);
+  const navigate = useNavigate();
+  return (
+      API: Promise<any>,
+      onResponse: (res: any) => void,
+      onError?: (code: ErrorCode, message: string, redirect?: string) => void
+    ) =>
+    async () => {
+      try {
+        const res = await API;
+        onResponse(res);
+      } catch (error) {
+        const { error: errorResponse } =
+          ((error as AxiosError)?.response?.data as { error: ErrorResponse }) ??
+          {};
+        const { code, message, redirect, clearAccessToken } = errorResponse;
+
+        console.error(message);
+
+        if (clearAccessToken) {
+          setAccessToken('');
+        }
+
+        if (onError) {
+          onError(code, message, redirect);
+        }
+
+        alert(errorMap[code]);
+
+        if (redirect) {
+          navigate(redirect);
+        }
+      }
+    };
+}
+
+export default useAPI;
