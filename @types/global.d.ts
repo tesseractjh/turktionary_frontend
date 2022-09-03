ff42e16e6f64c2f1710ddb80a8731dc7483dd021
Tesseract <tensai3355@gmail.com>
2022-09-04 03:03:04 +0900

    :label: 에러 타입 추가

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -6,6 +6,14 @@ declare global {
   type ThemeName = 'LIGHT' | 'DARK';
   type DictionaryType = 'ALL' | 'TR' | 'AZ' | 'UZ' | 'KZ' | 'TM' | 'KG';
   type ButtonSize = 'small' | 'medium' | 'big';
+  type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+  type ErrorCode = `${Digit}${Digit}${Digit}`;
+  type ErrorResponse = {
+    code: ErrorCode;
+    message: string;
+    redirect?: string;
+    clearAccessToken?: boolean;
+  };
 
   interface ThemeGroup {
     LIGHT: Theme;
