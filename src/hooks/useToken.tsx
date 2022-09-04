cdbfe65dba13561b6a8a53a425a18eac5579bc0f
Tesseract <tensai3355@gmail.com>
2022-09-04 11:55:53 +0900

    :bug: App이 리렌더될 때마다 refresh 요청을 보내는 버그 수정

--- src/hooks/useToken.tsx
+++ src/hooks/useToken.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import userAPI from '@api/user';
 import useSetAccessToken from '@hooks/useSetAccessToken';
 import useAPI from './useAPI';
@@ -7,13 +7,15 @@ const useToken = () => {
   const api = useAPI();
   const setAccessToken = useSetAccessToken();
 
+  const initiateAccessToken = useCallback(async () => {
+    await api(userAPI.getAccessToken(), ({ accessToken }) =>
+      setAccessToken(accessToken)
+    )();
+  }, []);
+
   useEffect(() => {
-    (async () => {
-      await api(userAPI.getAccessToken(), ({ accessToken }) =>
-        setAccessToken(accessToken)
-      )();
-    })();
-  });
+    initiateAccessToken();
+  }, []);
 };
 
 export default useToken;
