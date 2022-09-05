50348b5cce54f7a6fd968ac30405cb5445031d63
Tesseract <tensai3355@gmail.com>
2022-09-05 17:36:08 +0900

    :heavy_plus_sign: react-query 의존성 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import GlobalStyle from '@styles/GlobalStyle';
 import ThemeProvider from '@contexts/ThemeProvider';
 import Login from '@pages/Login';
@@ -9,32 +10,39 @@ import Dictionary from '@pages/Main/Dictionary';
 import JoinForm from '@pages/Join/JoinForm';
 import JoinSuccess from '@pages/Join/JoinSuccess';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <>
       <GlobalStyle />
       <RecoilRoot>
-        <ThemeProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route element={<Main />}>
-                <Route index element={<Dictionary type="ALL" />} />
-                <Route path="turkish" element={<Dictionary type="TR" />} />
-                <Route path="azerbaijani" element={<Dictionary type="AZ" />} />
-                <Route path="uzbek" element={<Dictionary type="UZ" />} />
-                <Route path="kazakh" element={<Dictionary type="KZ" />} />
-                <Route path="turkmen" element={<Dictionary type="TM" />} />
-                <Route path="kyrgyz" element={<Dictionary type="KG" />} />
-                <Route path="*" element={<div>Not Found</div>} />
-              </Route>
-              <Route path="login" element={<Login />} />
-              <Route element={<Join />}>
-                <Route path="join/form" element={<JoinForm />} />
-                <Route path="join/success" element={<JoinSuccess />} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route element={<Main />}>
+                  <Route index element={<Dictionary type="ALL" />} />
+                  <Route path="turkish" element={<Dictionary type="TR" />} />
+                  <Route
+                    path="azerbaijani"
+                    element={<Dictionary type="AZ" />}
+                  />
+                  <Route path="uzbek" element={<Dictionary type="UZ" />} />
+                  <Route path="kazakh" element={<Dictionary type="KZ" />} />
+                  <Route path="turkmen" element={<Dictionary type="TM" />} />
+                  <Route path="kyrgyz" element={<Dictionary type="KG" />} />
+                  <Route path="*" element={<div>Not Found</div>} />
+                </Route>
+                <Route path="login" element={<Login />} />
+                <Route element={<Join />}>
+                  <Route path="join/form" element={<JoinForm />} />
+                  <Route path="join/success" element={<JoinSuccess />} />
+                </Route>
+              </Routes>
+            </BrowserRouter>
+          </ThemeProvider>
+        </QueryClientProvider>
       </RecoilRoot>
     </>
   );
