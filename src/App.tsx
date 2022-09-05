a1606e9c6eaaac97f326f07774e0fdcce1cc9a1d
Tesseract <tensai3355@gmail.com>
2022-09-05 16:29:36 +0900

    :adhesive_bandage: GlobalStyle, RecoilRoot, Provider 등을 App 컴포넌트로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import GlobalStyle from '@styles/GlobalStyle';
+import ThemeProvider from '@contexts/ThemeProvider';
 import Login from '@pages/Login';
 import Join from '@pages/Join';
 import Main from '@pages/Main';
@@ -8,23 +11,32 @@ import JoinSuccess from '@pages/Join/JoinSuccess';
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Main />}>
-        <Route index element={<Dictionary type="ALL" />} />
-        <Route path="turkish" element={<Dictionary type="TR" />} />
-        <Route path="azerbaijani" element={<Dictionary type="AZ" />} />
-        <Route path="uzbek" element={<Dictionary type="UZ" />} />
-        <Route path="kazakh" element={<Dictionary type="KZ" />} />
-        <Route path="turkmen" element={<Dictionary type="TM" />} />
-        <Route path="kyrgyz" element={<Dictionary type="KG" />} />
-        <Route path="*" element={<div>Not Found</div>} />
-      </Route>
-      <Route path="login" element={<Login />} />
-      <Route element={<Join />}>
-        <Route path="join/form" element={<JoinForm />} />
-        <Route path="join/success" element={<JoinSuccess />} />
-      </Route>
-    </Routes>
+    <>
+      <GlobalStyle />
+      <RecoilRoot>
+        <ThemeProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route element={<Main />}>
+                <Route index element={<Dictionary type="ALL" />} />
+                <Route path="turkish" element={<Dictionary type="TR" />} />
+                <Route path="azerbaijani" element={<Dictionary type="AZ" />} />
+                <Route path="uzbek" element={<Dictionary type="UZ" />} />
+                <Route path="kazakh" element={<Dictionary type="KZ" />} />
+                <Route path="turkmen" element={<Dictionary type="TM" />} />
+                <Route path="kyrgyz" element={<Dictionary type="KG" />} />
+                <Route path="*" element={<div>Not Found</div>} />
+              </Route>
+              <Route path="login" element={<Login />} />
+              <Route element={<Join />}>
+                <Route path="join/form" element={<JoinForm />} />
+                <Route path="join/success" element={<JoinSuccess />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </RecoilRoot>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
 import axios from 'axios';
-import GlobalStyle from '@styles/GlobalStyle';
-import ThemeProvider from '@contexts/ThemeProvider';
 import App from './App';
 
 axios.defaults.baseURL = process.env.API_URL;
@@ -15,15 +11,4 @@ if (!rootNode) {
   throw new Error('root 노드를 찾을 수 없습니다');
 }
 
-ReactDOM.createRoot(rootNode).render(
-  <>
-    <GlobalStyle />
-    <RecoilRoot>
-      <ThemeProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
-    </RecoilRoot>
-  </>
-);
+ReactDOM.createRoot(rootNode).render(<App />);
