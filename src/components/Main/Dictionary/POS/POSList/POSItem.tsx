e60a27a8ce8ab15662a5e95cb46508cfa1d60a8f
Tesseract <tensai3355@gmail.com>
2022-09-21 18:25:38 +0900

    :truck: 메뉴 버튼을 공통 컴포넌트로 분리

--- src/components/Main/Dictionary/POS/POSList/POSItem.tsx
+++ src/components/Main/Dictionary/POS/POSList/POSItem.tsx
@@ -3,8 +3,7 @@ import styled from '@emotion/styled';
 import { border, flex } from '@styles/minxin';
 import useLanguage from '@hooks/useLanguage';
 import pxToRem from '@utils/pxToRem';
-import MenuIcon from '@assets/images/ellipsis-vertical-solid.svg';
-import { Color } from '@emotion/react';
+import Button from '../../../../common/EllipsisMenu';
 
 interface POSItemProps {
   title: string;
@@ -23,72 +22,6 @@ const Container = styled.li`
 const Content = styled.div`
   flex-grow: 1;
   width: 100%;
-`;
-
-const ButtonContainer = styled.div`
-  align-self: flex-start;
-  position: relative;
-  margin-left: ${pxToRem(20)};
-  text-align: right;
-
-  &:hover {
-    width: ${pxToRem(80)};
-  }
-
-  &:hover > div {
-    display: block;
-  }
-
-  &:hover svg {
-    fill: ${({ theme }) => theme.color.GRAY};
-  }
-`;
-
-const Ellipsis = styled.span`
-  display: inline-block;
-  width: ${pxToRem(20)};
-  height: ${pxToRem(20)};
-
-  & svg {
-    width: 100%;
-    height: 100%;
-    fill: ${({ theme }) => theme.color.BORDER};
-  }
-`;
-
-const Hover = styled.div`
-  display: none;
-  position: absolute;
-  top: 0;
-  right: 0;
-  width: ${pxToRem(80)};
-  height: ${pxToRem(30)};
-  background-color: transparent;
-`;
-
-const Menu = styled.div`
-  display: none;
-  position: absolute;
-  top: ${pxToRem(30)};
-  right: 0;
-  z-index: 10;
-  width: ${pxToRem(80)};
-  background-color: ${({ theme }) => theme.color.WHITE};
-  box-shadow: 0 2px 5px rgb(0 0 0 / 17%);
-`;
-
-const MenuButton = styled.button<{ textColor?: keyof Color }>`
-  width: 100%;
-  height: ${pxToRem(40)};
-  padding: ${pxToRem(10)};
-  background-color: ${({ theme }) => theme.color.WHITE};
-  font-size: ${({ theme }) => theme.fontSize.sm};
-  color: ${({ textColor, theme }) => theme.color[textColor ?? 'BLACK']};
-  text-align: left;
-
-  &:hover {
-    background-color: ${({ theme }) => theme.color.BORDER};
-  }
 `;
 
 const Title = styled.h4`
@@ -150,18 +83,7 @@ function POSItem({ title, text, examples, exampleOrders }: POSItemProps) {
           </ExampleList>
         ) : null}
       </Content>
-      <ButtonContainer>
-        <Ellipsis>
-          <MenuIcon />
-        </Ellipsis>
-        <Hover />
-        <Menu>
-          <MenuButton type="button">편집</MenuButton>
-          <MenuButton type="button" textColor="RED">
-            신고
-          </MenuButton>
-        </Menu>
-      </ButtonContainer>
+      <Button posName={title} />
     </Container>
   );
 }
---
+++ src/components/common/EllipsisMenu.tsx
@@ -0,0 +1,95 @@
+import { Link } from 'react-router-dom';
+import { Color } from '@emotion/react';
+import styled from '@emotion/styled';
+import pxToRem from '@utils/pxToRem';
+import MenuIcon from '@assets/images/ellipsis-vertical-solid.svg';
+
+interface EllipsisMenuProps {
+  posName: string;
+}
+
+const Container = styled.div`
+  align-self: flex-start;
+  position: relative;
+  margin-left: ${pxToRem(20)};
+  text-align: right;
+
+  &:hover {
+    width: ${pxToRem(80)};
+  }
+
+  &:hover > div {
+    display: block;
+  }
+
+  &:hover svg {
+    fill: ${({ theme }) => theme.color.GRAY};
+  }
+`;
+
+const Ellipsis = styled.span`
+  display: inline-block;
+  width: ${pxToRem(20)};
+  height: ${pxToRem(20)};
+
+  & svg {
+    width: 100%;
+    height: 100%;
+    fill: ${({ theme }) => theme.color.BORDER};
+  }
+`;
+
+const Hover = styled.div`
+  display: none;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: ${pxToRem(80)};
+  height: ${pxToRem(30)};
+  background-color: transparent;
+`;
+
+const Menu = styled.div`
+  display: none;
+  position: absolute;
+  top: ${pxToRem(30)};
+  right: 0;
+  z-index: 10;
+  width: ${pxToRem(80)};
+  background-color: ${({ theme }) => theme.color.WHITE};
+  box-shadow: 0 2px 5px rgb(0 0 0 / 17%);
+`;
+
+const MenuButton = styled(Link)<{ textcolor?: keyof Color }>`
+  display: inline-block;
+  width: 100%;
+  height: ${pxToRem(40)};
+  padding: ${pxToRem(10)};
+  background-color: ${({ theme }) => theme.color.WHITE};
+  font-size: ${({ theme }) => theme.fontSize.sm};
+  color: ${({ textcolor, theme }) => theme.color[textcolor ?? 'BLACK']};
+  text-align: left;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.BORDER};
+  }
+`;
+
+function EllipsisMenu({ posName }: EllipsisMenuProps) {
+  return (
+    <Container>
+      <Ellipsis>
+        <MenuIcon />
+      </Ellipsis>
+      <Hover />
+      <Menu>
+        <MenuButton to={`edit/${posName}`}>편집</MenuButton>
+        <MenuButton to={`report/${posName}`} textcolor="RED">
+          신고
+        </MenuButton>
+      </Menu>
+    </Container>
+  );
+}
+
+export default EllipsisMenu;
