730a042d709f1ff7508ba840621b46d50e1c6481
Tesseract <tensai3355@gmail.com>
2022-10-12 02:06:03 +0900

    :sparkles: 단어 추가를 위한 항목들의 구역을 나누기 위한 wrapper 추가

--- src/components/Main/Dictionary/Vocabulary/VocaEdit/index.tsx
+++ src/components/Main/Dictionary/Vocabulary/VocaEdit/index.tsx
@@ -1,12 +1,23 @@
 import DictContentContainer from '@components/common/DictContentContainer';
+import styled from '@emotion/styled';
 import useLogin from '@hooks/api/useLogin';
 import useLanguage from '@hooks/useLanguage';
+import { border } from '@styles/minxin';
+import pxToRem from '@utils/pxToRem';
 import Form from '../../Form';
 import VocaSelection from '../../Form/VocaSelection';
 
 interface VocaEditProps {
   isCreate?: boolean;
 }
+
+const Wrapper = styled.div`
+  &:not(:last-of-type) {
+    padding-bottom: ${pxToRem(20)};
+    margin-bottom: ${pxToRem(20)};
+    border-bottom: ${border()} ${({ theme }) => theme.color.GRAY_LIGHT};
+  }
+`;
 
 function VocaEdit({ isCreate }: VocaEditProps) {
   const { langId, langName } = useLanguage();
@@ -20,16 +31,17 @@ function VocaEdit({ isCreate }: VocaEditProps) {
     <DictContentContainer
       title={`${langName} 어휘 ${isCreate ? '추가' : '편집'}`}
     >
-      <Form
-        id={`${langId}-voca-headword`}
-        label="표제어"
-        maxLength={255}
-        placeholder="사전에 등재될 표제어를 입력해주세요."
-      />
-      <VocaSelection id="synonym" label="유의어" />
-      <VocaSelection id="antonym" label="반의어" />
-      <VocaSelection id="cognate" label="동원어" findAll />
-      <div style={{ marginBottom: 500 }}></div>
+      <Wrapper>
+        <Form
+          id={`${langId}-voca-headword`}
+          label="표제어"
+          maxLength={255}
+          placeholder="사전에 등재될 표제어를 입력해주세요."
+        />
+        <VocaSelection id="synonym" label="유의어" />
+        <VocaSelection id="antonym" label="반의어" />
+        <VocaSelection id="cognate" label="동원어" findAll />
+      </Wrapper>
     </DictContentContainer>
   );
 }
