daff519567f89120653eba5af009bab412038899
Tesseract <tensai3355@gmail.com>
2022-10-19 01:33:17 +0900

    :adhesive_bandage: 유효한 값만 반환하도록 변경

--- src/utils/queryString.ts
+++ src/utils/queryString.ts
@@ -1,9 +1,9 @@
 const queryString = (obj: object) =>
   Object.entries(obj)
-    .map(
-      ([key, value]) =>
-        `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+    .map(([key, value]) =>
+      value ? `${encodeURIComponent(key)}=${encodeURIComponent(value)}` : null
     )
+    .filter(Boolean)
     .join('&');
 
 export default queryString;
